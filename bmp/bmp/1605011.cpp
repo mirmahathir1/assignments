ac935537ec6e2db60760e63491112d14b255df8f
Mahathir <mirmahathir1@gmail.com>
2021-04-18 19:24:41 +0600

    bmp: completed all matrix generation

--- bmp/bmp/1605011.cpp
+++ bmp/bmp/1605011.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <math.h>
+#define pi (2 * acos(0.0))
 using namespace std;
+
+double degreeToRadian(double degree)
+{
+	return (degree * (pi / 180.0));
+}
 
 class TwoDArray
 {
@@ -87,9 +95,11 @@ public:
 
 class OneDArray : public TwoDArray
 {
-protected:
+public:
     OneDArray() : TwoDArray(4, 1) {}
-    OneDArray(OneDArray *a) : TwoDArray(a) {}
+    OneDArray(OneDArray *a) : TwoDArray(a) {
+        this->set(3,1);
+    }
 
 public:
     void set(int index, double value)
@@ -116,6 +126,20 @@ public:
             product = product + a->get(i) * b->get(i);
         return product;
     }
+    OneDArray* multiply(double factor){
+        OneDArray* returnArray = new OneDArray(this);
+        returnArray->set(0,returnArray->get(0)*factor);
+        returnArray->set(1,returnArray->get(1)*factor);
+        returnArray->set(2,returnArray->get(2)*factor);
+        return returnArray;
+    }
+    OneDArray* sum(OneDArray* a){
+        OneDArray* returnArray = new OneDArray(this);
+        returnArray->set(0,returnArray->get(0)+a->get(0));
+        returnArray->set(1,returnArray->get(1)+a->get(1));
+        returnArray->set(2,returnArray->get(2)+a->get(2));
+        return returnArray;
+    }
 };
 
 class Matrix : public TwoDArray
@@ -138,33 +162,107 @@ class Vector : public OneDArray
 public:
     Vector() : OneDArray() {}
     Vector(Vector *a) : OneDArray(a) {}
+    static OneDArray* rotate(OneDArray* x, Vector* a, double theta){
+        
+        double cosineOfDegree = cos((double)degreeToRadian(theta));
+		double sineOfDegree = sin((double)degreeToRadian(theta));
+        
+        OneDArray* sumVector = new OneDArray();
+        sumVector = sumVector->sum(x->multiply(cosineOfDegree));
+        sumVector = sumVector->sum(a->multiply(OneDArray::dotProduct(a,x))->multiply(1-cosineOfDegree));
+        sumVector = sumVector->sum(OneDArray::crossProduct(a,x)->multiply(sineOfDegree));
+        return sumVector;
+    }
+
+    static Matrix* generateRotationMatrix(Vector* a, double angle){
+        Matrix* resultMatrix = new Matrix();
+        
+        Vector* i = new Vector(); i->set(0,1); i->set(3,1);
+        Vector* j = new Vector(); j->set(1,1); j->set(3,1);
+        Vector* k = new Vector(); k->set(2,1); k->set(3,1);
+        
+        OneDArray* c1; OneDArray* c2;OneDArray* c3;
+        Vector* normalized_a = a->normalize();
+
+        
+
+        c1 = Vector::rotate(i,normalized_a,angle);
+        c2 = Vector::rotate(j,normalized_a,angle);
+        c3 = Vector::rotate(k,normalized_a,angle);
+
+        resultMatrix->set(0,0,c1->get(0));
+        resultMatrix->set(0,1,c2->get(0));
+        resultMatrix->set(0,2,c3->get(0));
+
+        resultMatrix->set(1,0,c1->get(1));
+        resultMatrix->set(1,1,c2->get(1));
+        resultMatrix->set(1,2,c3->get(1));
+
+        resultMatrix->set(2,0,c1->get(2));
+        resultMatrix->set(2,1,c2->get(2));
+        resultMatrix->set(2,2,c3->get(2));
+
+        resultMatrix->set(3,3,1);
+        
+
+        return resultMatrix;
+    }
+
+    Vector* normalize(){
+        Vector* result = new Vector(this);
+
+        double denominator = sqrt(result->get(0)*result->get(0)+result->get(1)*result->get(1)+result->get(2)*result->get(2));
+        result->set(0,result->get(0)/denominator);
+        result->set(1,result->get(1)/denominator);
+        result->set(2,result->get(2)/denominator);
+
+        return result;
+    }
+
+
 };
 class Point : public OneDArray
 {
 public:
     Point() : OneDArray() {}
     Point(Point *a) : OneDArray(a) {}
+    void normalize()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            this->set(i, this->get(i) / this->get(3));
+        }
+    }
 };
 
-class Stack{
+class Stack
+{
 private:
-    vector<Matrix*> matrixStack;
+    vector<Matrix *> matrixStack;
+
 public:
-    void push(Matrix* a){
+    void push(Matrix *a)
+    {
         matrixStack.push_back(a);
     }
-    Matrix* pop(){
-        if(isEmpty()){
+    Matrix *pop()
+    {
+        if (isEmpty())
+        {
             cout << "ERROR: Trying to POP from stack with no element" << endl;
             exit(0);
         }
 
-        Matrix* popped = matrixStack[matrixStack.size()-1];
+        Matrix *popped = matrixStack[matrixStack.size() - 1];
         matrixStack.pop_back();
         return popped;
     }
-    bool isEmpty(){
-        return matrixStack.size()==0;
+    bool isEmpty()
+    {
+        return matrixStack.size() == 0;
+    }
+    Matrix* get_top(){
+        return matrixStack[matrixStack.size() - 1];
     }
 };
 
@@ -172,32 +270,151 @@ Stack stack;
 
 int main()
 {
+    ifstream fin;
+    fin.open("scene.txt");
 
-    Matrix *a = new Matrix();
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            //a->data[i][j]= i+1;
-            a->set(i, j, i + 1);
-        }
+    Point *eye = new Point();
+    Vector *look = new Vector();
+    Vector *up = new Vector();
+
+    double fovY, aspectRatio, near, far;
+    double numberInput;
+
+    fin >> numberInput;
+    eye->set(0, numberInput);
+    fin >> numberInput;
+    eye->set(1, numberInput);
+    fin >> numberInput;
+    eye->set(2, numberInput);
+    eye->set(3, 1);
+    fin >> numberInput;
+    look->set(0, numberInput);
+    fin >> numberInput;
+    look->set(1, numberInput);
+    fin >> numberInput;
+    look->set(2, numberInput);
+    look->set(3, 1);
+    fin >> numberInput;
+    up->set(0, numberInput);
+    fin >> numberInput;
+    up->set(1, numberInput);
+    fin >> numberInput;
+    up->set(2, numberInput);
+    up->set(3, 1);
+    fin >> fovY >> aspectRatio >> near >> far;
+
+    eye->print();
+    look->print();
+    up->print();
+    cout << fovY << " " << aspectRatio << " " << near << " " << far << endl;
+
+    Matrix* initMatrix = new Matrix();
+    for(int i = 0 ; i < 4; i++){
+        initMatrix->set(i,i,1);
     }
-
-    Matrix *b = new Matrix(a);
-
-    Matrix *c = new Matrix((Matrix *)TwoDArray::multiply(a, b));
-
     
+    stack.push(initMatrix);
 
-    stack.push(a);
-    a->print();
-    stack.push(b);
-    b->print();
-    stack.push(c);
-    c->print();
-    cout<<"POPPING"<<endl;
-    while(!stack.isEmpty()){
-        Matrix* popped = stack.pop();
-        popped->print();
+    string option;
+    while (true)
+    {
+        fin >> option;
+        if (option == "triangle")
+        {
+            cout << "Triangle found" << endl;
+            Point *point1 = new Point();
+            Point *point2 = new Point();
+            Point *point3 = new Point();
+            fin >> numberInput;
+            point1->set(0, numberInput);
+            fin >> numberInput;
+            point1->set(1, numberInput);
+            fin >> numberInput;
+            point1->set(2, numberInput);
+            point1->set(3, 1);
+            fin >> numberInput;
+            point2->set(0, numberInput);
+            fin >> numberInput;
+            point2->set(1, numberInput);
+            fin >> numberInput;
+            point2->set(2, numberInput);
+            point2->set(3, 1);
+            fin >> numberInput;
+            point3->set(0, numberInput);
+            fin >> numberInput;
+            point3->set(1, numberInput);
+            fin >> numberInput;
+            point3->set(2, numberInput);
+            point3->set(3, 1);
+
+            
+            point1->print();
+            point2->print();
+            point3->print();
+        }
+        else if (option == "scale")
+        {
+            cout << "Scale found" << endl;
+            Matrix *scaleMatrix = new Matrix();
+            fin >> numberInput;
+            scaleMatrix->set(0, 0, numberInput);
+            fin >> numberInput;
+            scaleMatrix->set(1, 1, numberInput);
+            fin >> numberInput;
+            scaleMatrix->set(2, 2, numberInput);
+            scaleMatrix->set(3, 3, 1);
+            scaleMatrix->print();
+        }
+        else if (option == "translate")
+        {
+            cout << "Translate found" << endl;
+            Matrix *translateMatrix = new Matrix();
+            fin >> numberInput;
+            translateMatrix->set(0, 3, numberInput);
+            fin >> numberInput;
+            translateMatrix->set(1, 3, numberInput);
+            fin >> numberInput;
+            translateMatrix->set(2, 3, numberInput);
+            translateMatrix->set(0, 0, 1);
+            translateMatrix->set(1, 1, 1);
+            translateMatrix->set(2, 2, 1);
+            translateMatrix->set(3, 3, 1);
+            translateMatrix->print();
+        }
+        else if (option == "rotate")
+        {
+            cout << "Rotate found" << endl;
+            Vector* axis = new Vector();
+            double angle;
+            fin >> angle;
+            fin >> numberInput;
+            axis->set(0,numberInput);
+            fin >> numberInput; 
+            axis->set(1,numberInput);
+            fin >> numberInput; 
+            axis->set(2,numberInput);
+            axis->set(3,1);
+
+            
+            
+            Matrix* rotationMatrix = Vector::generateRotationMatrix(axis,angle);
+
+            rotationMatrix->print();
+            
+          
+        }
+        else if (option == "push")
+        {
+            cout << "Push found" << endl;
+        }
+        else if (option == "pop")
+        {
+            cout << "Pop found" << endl;
+        }
+        else if (option == "end")
+        {
+            cout<< "End found" << endl;
+            break;
+        }
     }
 }
